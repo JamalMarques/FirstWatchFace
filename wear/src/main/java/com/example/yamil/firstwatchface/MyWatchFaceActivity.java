795e3134da8beab11c739e6131c251ec2de10e18
JamalMarques <yamamarques@gmail.com>
2015-02-03 16:53:46 -0300

    First commit

--- wear/src/main/java/com/example/yamil/firstwatchface/MyWatchFaceActivity.java
+++ wear/src/main/java/com/example/yamil/firstwatchface/MyWatchFaceActivity.java
@@ -8,6 +8,7 @@ import android.content.IntentFilter;
 import android.graphics.Typeface;
 import android.os.BatteryManager;
 import android.os.Bundle;
+import android.service.wallpaper.WallpaperService;
 import android.support.wearable.view.WatchViewStub;
 import android.support.wearable.watchface.CanvasWatchFaceService;
 import android.widget.TextView;
@@ -18,7 +19,7 @@ import java.util.Calendar;
 public class MyWatchFaceActivity extends Activity {
 
     //private static final String baseAssetsAccess = "fonts/Roboto/";
-    private static final String typeface = "fonts/Roboto/Roboto-BlackItalic.ttf";
+    private static final String typeface = "fonts/Roboto/Roboto-Medium.ttf";
 
     private static final IntentFilter INTENT_FILTER;  //Intent that matches with some of your Actions will be detected by broadcast
     static{
@@ -28,13 +29,14 @@ public class MyWatchFaceActivity extends Activity {
         INTENT_FILTER.addAction(Intent.ACTION_TIME_CHANGED);
     }
 
-    private String time_format_display = "hh:mm";
-    private TextView mTime,mBattery;
+    private String time_format_display = "mm";
+    private TextView mTimeHours,mTimeMinutes,mBattery;
 
     private BroadcastReceiver mTimeInfoReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            mTime.setText(new SimpleDateFormat(time_format_display).format(Calendar.getInstance().getTime()));
+            mTimeHours.setText(String.valueOf(Calendar.getInstance().get(Calendar.HOUR_OF_DAY))/*new SimpleDateFormat(time_format_display).format(Calendar.getInstance().getTime())*/);
+            mTimeMinutes.setText(new SimpleDateFormat(time_format_display).format(Calendar.getInstance().getTime())/*String.valueOf(Calendar.getInstance().get(Calendar.MINUTE))*/);
         }
     };
 
@@ -54,9 +56,11 @@ public class MyWatchFaceActivity extends Activity {
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                mTime = (TextView) stub.findViewById(R.id.watch_time);
+                mTimeHours = (TextView) stub.findViewById(R.id.watch_time_hours);
+                mTimeMinutes = (TextView) stub.findViewById(R.id.watch_time_minutes);
                 mBattery = (TextView) stub.findViewById(R.id.watch_battery);
-                setTypeface(mTime);
+                setTypeface(mTimeHours);
+                setTypeface(mTimeMinutes);
                 setTypeface(mBattery);
 
                 mTimeInfoReceiver.onReceive(MyWatchFaceActivity.this, registerReceiver(null,INTENT_FILTER));
@@ -64,6 +68,7 @@ public class MyWatchFaceActivity extends Activity {
                 registerReceiver(mBatteryInfoReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
             }
         });
+
     }
 
     private void setTypeface(TextView textView){
